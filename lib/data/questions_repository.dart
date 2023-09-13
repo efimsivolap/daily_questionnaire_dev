d6ffe23966bdc3bbf9942f8465737ca5e18f417c
pack_dev <i.pack.dev@gmail.com>
2023-09-13 19:21:36 +0300

    fix: gitignore and json, and lint

--- lib/data/questions_repository.dart
+++ lib/data/questions_repository.dart
@@ -9,10 +9,9 @@ class QuestionsRepository {
 
   Future<List<QuestionModel>> getQuestions() async {
     final String source = await rootBundle.loadString('assets/questions.json');
-    final List<Map<String, dynamic>> data =
-        List<Map<String, dynamic>>.from(json.decode(source));
-    print(data);
-    return data.map((e) => QuestionModel.fromJson(e)).toList();
+    final decoded =
+        (await json.decode(source) as List).cast<Map<String, dynamic>>();
+    return decoded.map(QuestionModel.fromJson).toList();
   }
 
   Future<void> postQuestions(List<QuestionModel> questions) async {}
