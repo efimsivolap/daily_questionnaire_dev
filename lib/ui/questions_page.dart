aaacbda95747b9c34ada0b6b75bf49037447cfe1
Efim Sivolap <efimsivolap@MacBook-Pro-Efim.local>
2023-09-15 17:52:13 +0530

    test1

--- lib/ui/questions_page.dart
+++ lib/ui/questions_page.dart
@@ -50,7 +50,7 @@ class QuestionsWidget extends StatelessWidget {
                   color: Colors.black,
                   fontSize: 32,
                   fontWeight: FontWeight.w600,
-                  height: 0.63,
+                  height: 0.62,
                 ),
               ),
               const SizedBox(height: 8),
